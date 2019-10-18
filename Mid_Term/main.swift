65e109928256ca8ea8758a177a5a402757246e84
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-10-17 20:56:48 -0400

    Initial Commit

---
+++ Mid_Term/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mid_Term
+//
+//  Created by MacStudent on 2019-10-17.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
